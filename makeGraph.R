150b22eeb145f82da05b2f38aebf537ebacbb35a
Mark Fickett <mark.fickett@gmail.com>
2011-01-23 10:07:49 -0500

    Output kilometers when graphing (as well as for trip meters).

--- makeGraph.R
+++ makeGraph.R
@@ -22,8 +22,8 @@ title <- paste(args[5:length(args)], collapse=" ")
 METERS_PER_KM <- 1000.0
 MILES_PER_KM <- 0.621371192
 MS_PER_HOUR <- 60*60*1000.0
-## ms interval to mph
-convertUnits <- function(milliseconds) ((METERS_PER_REVOLUTION * (1/METERS_PER_KM) * MILES_PER_KM) / (milliseconds * (1/MS_PER_HOUR)) )
+## ms interval to km/h
+convertUnits <- function(milliseconds) ((METERS_PER_REVOLUTION * (1/METERS_PER_KM)) / (milliseconds * (1/MS_PER_HOUR)) )
 
 xmin <- intervalData$intervals[length(intervalData$intervals)-1]
 xmax <- intervalData$intervals[1]
@@ -35,8 +35,8 @@ for(i in 1:length(intervalData$counts)) {
 }
 
 total <- sum(intervalData$counts)
-total <- total * METERS_PER_REVOLUTION * (1/METERS_PER_KM) * MILES_PER_KM
-title <- sprintf("%s: %.2f Miles", title, total)
+total <- total * METERS_PER_REVOLUTION * (1/METERS_PER_KM)
+title <- sprintf("%s: %.2f km", title, total)
 
-plot(convertUnits(intervalData$intervals), intervalData$counts, type='h', col="red", main=title, xlab="Speed (mph)", ylab="Frequency", log="x", xlim=convertUnits(c(xmax, xmin)), yaxt="n", lwd=3, lend="square")
+plot(convertUnits(intervalData$intervals), intervalData$counts, type='h', col="red", main=title, xlab="Speed (km/h)", ylab="Frequency", log="x", xlim=convertUnits(c(xmax, xmin)), yaxt="n", lwd=3, lend="square")
 
